0411b2ea97f6034e02f6c9e32c0a99c7e9d0f052
farahnaz <75225445+farahnazjafari@users.noreply.github.com>
2022-12-21 14:43:23 +0330

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a special "matrix" object that can cache 
+## its inverse.
 
-## Write a short comment describing this function
+## expected input: matrix object
+## expected output: list
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(X = matrix()) {
+    inv <- NULL
+    # if matrix is not set, set a matrix
+    set <- function(y){
+        X <<- y
+        inv <<- NULL
+    }
+    
+    # getting matrix
+    get <- function() X
+    
+    # setting and getting inverse of the matrix
+    setinv <- function(solve) inv <<- solve
+    getinv <- function() inv
+    
+    list(set=set, get=get,
+         setinv=setinv, 
+         getinv=getinv)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## expected input: list object
+## expected output: matrix that is the inverse of 'x'
+
+cacheSolve <- function(X, ...) {
+    
+    # geting invers
+    inv <- X$getinv()
+    
+    # checking if invers is done 
+    if(!is.null(inv)){
+        message("getting cached matrix")
+        return(inv)
+    }
+    
+    # getting matrix to the data
+    data <- X$get()
+    # inverting matrix
+    inv <- solve(data, ...)
+    # "saving" inverted matrix
+    X$setinv(inv)
+    # printing inverted matrix to the screen
+    print(inv)    
 }
